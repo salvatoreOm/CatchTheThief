8ed3b3cbb79f11f014e6bfd096add7620f762bd7
Om Pratap Singh Parihar <omparihar0001@gmail.com>
2024-03-20 14:12:09 +0530

    Initial Commit

---
+++ CatchTheThief/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchTheThief
+//
+//  Created by Om Parihar on 20/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
